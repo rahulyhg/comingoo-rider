97a5b2a5e72fcfd548e11392224120af2013dc99
Bhanu Sagar <vbhanusagar@gmail.com>
2019-03-29 21:36:29 +0530

    6-development-standards:created intial screen for rider app

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,35 +1,40 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet, 
-  Text, 
-  View,
-  Image,
-  Button
-} from 'react-native';
-
+import React, { Component } from "react";
+import { Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import { images } from "../../utils";
+import styles from "./styles";
 
 export default class Home extends Component {
   render() {
     return (
-      <View  style={styles.container}>
-        <Button
-          title="Login"
-          onPress={() =>
-            this.props.navigation.navigate('Login')
-          }
-        />
-        
-      </View>
+      <ScrollView style={styles.container} contentContainerStyle={{ flex: 1 }}>
+        <View style={styles.topContainer}>
+          <Image
+            source={images.comingoo_logo}
+            style={styles.logoStyles}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.middleContainer}>
+          <View style={styles.middleInnerContainer}>
+            <View style={styles.middleTextContainer}>
+              <Text style={styles.smallTxt}>Don't have an account?</Text>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate("Signup")}>
+                <Text style={styles.mediumTxt}>Sign up</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.bottomInnerContainer}>
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.accountColor}>Already have an account</Text>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate("Login")}>
+                <Text style={styles.signInColor}>Sign in</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-   
-  }
-});
