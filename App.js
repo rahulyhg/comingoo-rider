c91eae3fbb3f28703a8a076b65f5ee4f6ad9b1d6
Bhanu Sagar <vbhanusagar@gmail.com>
2019-03-29 08:41:43 +0530

    Small changes for navigation

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import AppNavigator from './src/navigations'
 export default class App extends Component {
   render() {
     return (
-        <AppNavigator />
+        <AppNavigator  />
     );
   }
 }
