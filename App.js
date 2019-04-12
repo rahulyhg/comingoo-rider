55e946e35ac6759620e512528da5d5314091dbea
Burak Sak <buraksakgit@gmail.com>
2019-04-12 23:33:46 +0300

    c75: Ignore warning. rn firebase author said: https://github.com/invertase/react-native-firebase/issues/1446#issuecomment-423365463

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { YellowBox } from "react-native";
 
 import Index from "./src/screens";
+
+YellowBox.ignoreWarnings(["Require cycle:"]);
 
 export default class App extends Component {
   render() {
